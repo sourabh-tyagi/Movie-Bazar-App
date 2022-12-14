508ae547887a3f37fbc531b0087a7117f09b7755
sourabh-tyagi <79514321+sourabh-tyagi@users.noreply.github.com>
2022-12-15 00:53:47 +0530

    Update UserRepository.java

--- Backend(Spring Boot)/src/main/java/com/example/loginuser/repository/UserRepository.java
+++ Backend(Spring Boot)/src/main/java/com/example/loginuser/repository/UserRepository.java
@@ -4,8 +4,6 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
-//import com.example.loginuser.dto.UserDto;
 import com.example.loginuser.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
