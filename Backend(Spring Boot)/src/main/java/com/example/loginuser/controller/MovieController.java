4ed9d0d73faa54a250fa0f1aa1c3e46a9946073a
sourabh-tyagi <79514321+sourabh-tyagi@users.noreply.github.com>
2022-12-15 00:49:58 +0530

    Update MovieController.java

--- Backend(Spring Boot)/src/main/java/com/example/loginuser/controller/MovieController.java
+++ Backend(Spring Boot)/src/main/java/com/example/loginuser/controller/MovieController.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.loginuser.dto.MovieDto;
@@ -23,15 +21,6 @@ public class MovieController {
 		return movieService.findAll(pageable);
 	}
 	
-//	@PostMapping("/addmovie")
-//	public String  NewMovie(@RequestBody MovieDto movieDto) throws Exception{
-//		boolean b=movieService.addMovie(movieDto);
-//		if(b)
-//			return "Movie Added Successfully";
-//		else
-//			return "Movie Failed To Add";
-//	}
-//	
 	
 	
 
